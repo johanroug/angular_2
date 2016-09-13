7ce4a3dbdd0551461cc882022e408fc0d5cc06c1
Johan Roug <joro@sdc.dk>
2016-09-13 09:06:21 +0200

    zippy

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,6 +4,12 @@ import {Component} from '@angular/core';
   selector: 'my-app',
   template: `
     <h1>Angular 2</h1>
+    <zippy title="This is the title">
+      Here is the content        
+    </zippy>
+    <zippy title="This is another the title">
+      Here is another content        
+    </zippy>
   `
 })
 export class AppComponent {}
--- app/app.module.ts
+++ app/app.module.ts
@@ -1,10 +1,11 @@
 import { NgModule }         from '@angular/core';
 import { BrowserModule }    from '@angular/platform-browser';
 import { AppComponent }     from './app.component';
+import {ZippyComponent} from "./zippy.component";
 
 @NgModule({
   imports:      [ BrowserModule ],
-  declarations: [ AppComponent ], // Components
+  declarations: [ AppComponent, ZippyComponent ], // Components
   bootstrap:    [ AppComponent ],
   providers:    [ ] // Services
 })
