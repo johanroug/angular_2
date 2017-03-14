2ba4efb5bca6ee21051439474587e29fdea0c77f
Johan Roug <joro@sdc.dk>
2017-03-14 13:22:55 +0100

    no message

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { BrowserModule  } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import { AppComponent }   from './app.component';
+import { ServiceComponent } from "./service/service.component";
+import { CmpAComponent } from "./service/cmp-a.component";
+import { CmpBComponent } from "./service/cmp-b.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        ServiceComponent,
+        CmpAComponent,
+        CmpBComponent
+    ],
+    imports: [BrowserModule],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
