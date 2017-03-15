863bf239d1a74c2e0b262e4068988d88311bd800
DESKTOP-IQ2GSM8\Johan <johanroug@gmail.com>
2017-03-15 22:51:06 +0100

    no message

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent }   from './app.component';
 import { ServiceComponent } from "./service/service.component";
 import { CmpAComponent } from "./service/cmp-a.component";
 import { CmpBComponent } from "./service/cmp-b.component";
+import { HttpComponent } from './http/http.component';
 
 @NgModule({
     declarations: [
         AppComponent,
         ServiceComponent,
         CmpAComponent,
-        CmpBComponent
+        CmpBComponent,
+        HttpComponent
     ],
     imports: [BrowserModule],
     providers: [],
---
+++ src/app/http/http.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-http',
+  templateUrl: './http.component.html'
+})
+export class HttpComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
