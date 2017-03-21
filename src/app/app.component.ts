365d916c7e8dbe4b45e91374d90a8d1b510e27bf
DESKTOP-IQ2GSM8\Johan <johanroug@gmail.com>
2017-03-22 00:27:59 +0100

    no message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TweenMax } from "gsap";
+
 
 @Component({
   selector: 'app-root',
@@ -6,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+
+  animateMe() {
+    let photo = document.getElementById("photo");
+    //TweenMax.to(photo, 2, {width:"200px", height:"550px"});
+
+    TweenMax.to(photo, 2, {width:"200px", height:"150px"});
+  }
 }
